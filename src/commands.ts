d0f6f53985cd9a27887a6b0413fe9d2c4351abfa
Leandro Motta Barros <lmb@stackedboxes.org>
2020-05-06 12:58:40 -0300

    Using `async`/`await` instead of `then`

--- src/commands.ts
+++ src/commands.ts
@@ -16,27 +16,27 @@ import * as fs from "fs";
  *
  * Used to (...drum roll...) create a note.
  */
-export function createNote() {
-	vscode.window.showInputBox({
+export async function createNote() {
+	let title = await vscode.window.showInputBox({
 		placeHolder: "Note title"
-	}).then(title => {
-		if (title === undefined) {
-			return;
-		}
-
-		let repo = vscode.workspace.getConfiguration().get<string>("zeka-vs-code.repository");
-		if (repo === undefined) {
-			return;
-		}
-
-		let cleanTitle = util.canonicalizeString(title);
-		let fileName = repo + "/notes/" + util.timestamp() + "-" + cleanTitle + ".md";
-
-		fs.writeFileSync(fileName, `# ${title}` + "\n\n");
-
-		var setting: vscode.Uri = vscode.Uri.parse("file:" + fileName);
-		vscode.workspace.openTextDocument(setting).then((doc: vscode.TextDocument) => {
-			vscode.window.showTextDocument(doc);
-		});
 	});
+
+	if (title === undefined) {
+		return;
+	}
+
+	let repo = vscode.workspace.getConfiguration().get<string>("zeka-vs-code.repository");
+	if (repo === undefined) {
+		return;
+	}
+
+	let cleanTitle = util.canonicalizeString(title);
+	let fileName = repo + "/notes/" + util.timestamp() + "-" + cleanTitle + ".md";
+
+	fs.writeFileSync(fileName, `# ${title}` + "\n\n");
+
+	let setting: vscode.Uri = vscode.Uri.parse("file:" + fileName);
+
+	let doc = await vscode.workspace.openTextDocument(setting);
+	vscode.window.showTextDocument(doc);
 }
